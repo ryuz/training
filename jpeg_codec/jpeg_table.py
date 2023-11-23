d3be207c14a96ec6a417de844f5577d7ebd4d839
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2023-11-23 18:58:35 +0900

    update

--- jpeg_codec/jpeg_table.py
+++ jpeg_codec/jpeg_table.py
@@ -426,3 +426,30 @@ for y in range(8):
             for u in range(8):
                 table_dct[v, u, y, x] = dct_weight(x, y, u, v)
 table_idct = table_dct.transpose(2, 3, 0, 1)
+
+
+tbl_zigzag = [
+    [ 0,  1,  5,  6, 14, 15, 27, 28],
+    [ 2,  4,  7, 13, 16, 26, 29, 42],
+    [ 3,  8, 12, 17, 25, 30, 41, 43],
+    [ 9, 11, 18, 24, 31, 40, 44, 53],
+    [10, 19, 23, 32, 39, 45, 52, 54],
+    [20, 22, 33, 38, 46, 51, 55, 60],
+    [21, 34, 37, 47, 50, 56, 59, 61],
+    [35, 36, 48, 49, 57, 58, 62, 63],
+]
+
+
+def make_zigzag():
+    a = []
+    p = []
+    for y in range(8):
+        for x in range(8):
+            a.append(tbl_zigzag[y][x])
+            p.append((x, y))
+    a = np.array(a)
+    p = np.array(p)
+    return p[np.argsort(a)]
+
+
+table_zigzag = make_zigzag()
